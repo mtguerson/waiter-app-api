98c91dc204f6fbf3b128ca3ea32b3746161791ec
Matheus Guerson <mtguerson@gmail.com>
2024-03-19 10:58:19 -0400

    connecting to mongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(() => console.log('Error connecting to MongoDB'));
 
 const port = 3001;
 
