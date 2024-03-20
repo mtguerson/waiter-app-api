85db53c774bf6b5192f1a00cf95c21fb0ba8f1ad
Matheus <mtguerson@gmail.com>
2024-03-19 22:08:53 -0400

    creating the cancelOrder feature

--- src/router.ts
+++ src/router.ts
@@ -9,6 +9,8 @@ import { createProduct } from './app/useCases/products/createProduct';
 import { listProductsByCategory } from './app/useCases/categories/listProductsByCategory';
 import { listOrders } from './app/useCases/orders/listOrders';
 import { createOrder } from './app/useCases/orders/createOrder';
+import { changeOrderStatus } from './app/useCases/orders/changeOrderStatus';
+import { cancelOrder } from './app/useCases/orders/cancelOrder';
 
 export const router = Router();
 
@@ -45,11 +47,7 @@ router.get('/orders', listOrders);
 router.post('/orders', createOrder);
 
 // Change Order Status
-router.patch('/orders/:orderId', (req, res) => {
-  res.send('OK');
-});
+router.patch('/orders/:orderId', changeOrderStatus);
 
 // Delete/Cancel Order
-router.delete('/categories/:orderId', (req, res) => {
-  res.send('OK');
-});
+router.delete('/orders/:orderId', cancelOrder);
